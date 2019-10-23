728c8eb82d7247c859274403653023fe7a003809
JiaweiZhuang <jiaweizhuang@g.harvard.edu>
2019-10-23 16:45:50 -0400

    Also test openmpi4 pt2pt

---
+++ scripts/pt2pt/pt2pt_openmpi4.sh
@@ -0,0 +1,17 @@
+if [ "$1" ]; then
+  LOG_DIR=$1
+else
+  echo 'Must specify output log directory'
+  exit 1
+fi
+
+mkdir -p $LOG_DIR
+
+spack env activate openmpi4
+cd $(spack location -i osu-micro-benchmarks)/libexec/osu-micro-benchmarks/mpi/pt2pt
+
+orterun ./osu_latency > $LOG_DIR/latency.log
+orterun ./osu_bw > $LOG_DIR/bw.log
+orterun ./osu_bibw > $LOG_DIR/bibw.log
+
+spack env deactivate
