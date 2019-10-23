5fc8de5e550d94e18f4d669eb1f7083262ee9b31
JiaweiZhuang <jiaweizhuang@g.harvard.edu>
2019-10-23 17:46:55 -0400

    Use correct mpirun command for MPICH

--- scripts/pt2pt/pt2pt_mpich3.sh
+++ scripts/pt2pt/pt2pt_mpich3.sh
@@ -10,8 +10,8 @@ mkdir -p $LOG_DIR
 spack env activate mpich3
 cd $(spack location -i osu-micro-benchmarks)/libexec/osu-micro-benchmarks/mpi/pt2pt
 
-orterun ./osu_latency > $LOG_DIR/latency.log
-orterun ./osu_bw > $LOG_DIR/bw.log
-orterun ./osu_bibw > $LOG_DIR/bibw.log
+mpirun ./osu_latency > $LOG_DIR/latency.log
+mpirun ./osu_bw > $LOG_DIR/bw.log
+mpirun ./osu_bibw > $LOG_DIR/bibw.log
 
 spack env deactivate
