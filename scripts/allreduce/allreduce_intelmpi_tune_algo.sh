24f26c89e770a1613757edb467cafa6cfc858d58
JiaweiZhuang <jiaweizhuang@g.harvard.edu>
2019-10-26 15:05:09 -0400

    Only run default algorithms for allreduce

--- scripts/allreduce/allreduce_intelmpi_tune_algo.sh
+++ scripts/allreduce/allreduce_intelmpi_tune_algo.sh
@@ -1,5 +1,6 @@
 if [ "$1" ]; then
   LOG_DIR=$1
+  mkdir -p $LOG_DIR
 else
   echo 'Must specify output log directory'
   exit 1
@@ -10,7 +11,19 @@ if [ -z $OSU_PATH_INTELMPI ]; then
   exit 1
 fi
 
-mkdir -p $LOG_DIR
+if [ -z $MPI_ALGO ]; then
+  MPI_ALGO='full'
+fi
+
+if [ $MPI_ALGO == 'full' ]; then
+  algos=$(seq 1 12)
+elif [ $MPI_ALGO == 'default' ]; then
+  algos=''
+else
+  echo 'Invalid $MPI_ALGO'
+  exit 1
+fi
+
 
 module purge
 module load intelmpi
@@ -21,7 +34,7 @@ cd $OSU_PATH_INTELMPI/libexec/osu-micro-benchmarks/mpi/collective
 unset I_MPI_ADJUST_ALLREDUCE
 mpirun ./osu_allreduce > $LOG_DIR/allreduce_default.log
 
-for algo in {1..12}; do
+for algo in $algos; do
     export I_MPI_ADJUST_ALLREDUCE=$algo
     mpirun ./osu_allreduce > $LOG_DIR/allreduce_algo${algo}.log
 done
--- scripts/allreduce/allreduce_openmpi3_tune_algo.sh
+++ scripts/allreduce/allreduce_openmpi3_tune_algo.sh
@@ -1,18 +1,31 @@
 if [ "$1" ]; then
   LOG_DIR=$1
+  mkdir -p $LOG_DIR
 else
   echo 'Must specify output log directory'
   exit 1
 fi
 
-mkdir -p $LOG_DIR
+
+if [ -z $MPI_ALGO ]; then
+  MPI_ALGO='full'
+fi
+
+if [ $MPI_ALGO == 'full' ]; then
+  algos=$(seq 0 5)
+elif [ $MPI_ALGO == 'default' ]; then
+  algos=''
+else
+  echo 'Invalid $MPI_ALGO'
+  exit 1
+fi
 
 spack env activate openmpi3
 cd $(spack location -i osu-micro-benchmarks)/libexec/osu-micro-benchmarks/mpi/collective
 
 orterun ./osu_allreduce > $LOG_DIR/allreduce_default.log
 
-for algo in {0..5}; do
+for algo in $algos; do
     orterun \
     --mca coll_tuned_use_dynamic_rules 1 \
     --mca coll_tuned_allreduce_algorithm $algo \
